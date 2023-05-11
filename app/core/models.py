f733ac4e57f383830e8ee514dfa0be2608e01a81
alfred <frealchar@gmail.com>
2023-05-11 20:58:53 +0800

    added models then migrate

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,10 @@
-# from django.db import models  #noqa
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    """Ingredient for recipes"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
