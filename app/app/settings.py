1a8ba8806e249b6f1031f5592e05d337027cdea2
alfred <frealchar@gmail.com>
2023-05-12 11:40:36 +0800

    migrate

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -116,3 +116,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AUTH_USER_MODEL = 'core.User'
--- app/core/models.py
+++ app/core/models.py
@@ -1,10 +1,22 @@
+"""
+Database models.
+"""
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 
-class Ingredient(models.Model):
-    """Ingredient for recipes"""
-    name = models.CharField(max_length=255)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE)
+class UserManager(BaseUserManager):
+    # use_in_migrations = True
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
