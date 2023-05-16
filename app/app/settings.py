60621778dbab887334ceb9b5484f4f9a51eef88d
alfred <frealchar@gmail.com>
2023-05-16 10:39:50 +0800

    build user api

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_spectacular',
     'core',
+    'user',
     ]
 
 MIDDLEWARE = [
@@ -119,3 +123,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
